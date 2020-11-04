d39f8f72a4e259a5e53f08811833742b1501f61b
Antti Salo <antti.salo@sc5.io>
2020-11-04 14:19:05 +0000

    overflow: hidden for the page wrapper

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledMain = styled.div``;
+const StyledMain = styled.div`
+  overflow: hidden;
+`;
 
 export default function Layout({ children }) {
   return <StyledMain>{children}</StyledMain>;
